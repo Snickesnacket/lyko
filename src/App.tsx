bad1d3a540e39d9a410382cf950694d7973f6f25
Astrid <astrid@avitusit.se>
2024-07-10 16:41:05 +0200

    first commit with content

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import hero from './assets/img.png'
+import './App.css'
+import React from "react";
+import perfumeImage from './assets/parfume.png'
+const  App: React.FC = () => {
+
+    const data = [
+        {
+            id: 1,
+            title: 'GIA',
+            brand: 'Stella McCartney',
+            img: perfumeImage,
+            price: 450,
+            currency: 'kr',
+            quantity: '50 ml',
+        },
+        {
+            id: 2,
+            title: 'AQUA DIO',
+            brand: 'Armani',
+            img: perfumeImage,
+            price: 450,
+            currency: 'kr',
+            quantity: '50 ml',
+        },
+        {
+            id: 3,
+            title: 'GIA',
+            brand: 'Stella McCartney',
+            img: perfumeImage,
+            price: 450,
+            currency: 'kr',
+            quantity: '50 ml',
+        },
+        {
+            id: 4,
+            title: 'AQUA DIO',
+            brand: 'Armani',
+            img: perfumeImage,
+            price: 450,
+            currency: 'kr',
+            quantity: '50 ml',
+        }
+    ]
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+          <header className=" w-full ">
+              <img className="w-full object-contain" src={hero}  alt="Vite logo"/>
+          </header>
+          <div className="flex flex-col items-center space-y-5 px-7">
+              <div className="flex items-center justify-center space-x-10">
+              <span>
+                  DAM
+              </span>
+                  <span>
+                  HERR
+              </span>
+              </div>
+              <h2 className="font-bold text-lg">Parfym</h2>
+              <div className="flex flex-col items-center max-w-[650px]">
+                  <span>Doftguide</span>
+                  <p className="text-center ">
+                      Det finns inget så härligt som att förälska sig i en ny doft, och i ett utbud så stort som vårt är
+                      det inte svårt att hitta nya älsklingar! Bland alla babes på denna sida finns magiska märken så
+                      som
+                      Ariana Grande, Versace, Clean, Dolce &amp; Gabbana, Hugo Boss, Escada med MÅNGA fler. Lycka till!
+                  </p>
+              </div>
+              <div className=" grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full gap-6 justify-items-center">
+                  {data && data.map((product) => (
+                          <div key={product.id} className="bg-modulebackground w-full h-full p-5">
+                              <img className="py-2 object-contain  h-full m-auto" src={product.img} alt=" product image"/>
+                          </div>
+                      )
+                  )}
+              </div>
+          </div>
+      </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
-
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#ffe',
+        modulebackground: 'rgb(242,243, 245)',
+      }
+    },
   },
   variants: {
     extend: {},
